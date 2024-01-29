7398300f77a3dcdcd3bf3b019222403a7903c1ff
MichelCode <michelseocode.com>
2024-01-29 16:31:37 +0100

    features test ajout migration

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Inertia\Inertia;
 | contains the "web" middleware group. Now create something great!
 |
 */
-// cc cc2 cc3
+//ccccccp
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
