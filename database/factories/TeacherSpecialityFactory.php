66bb60473830a89156b56e29aa422e340b1305c9
MichelCode <michelseocode.com>
2024-01-31 11:34:02 +0100

    Configuation des factories ( attention évaluation et teacher --)

--- database/factories/TeacherSpecialityFactory.php
+++ database/factories/TeacherSpecialityFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Speciality;
+use App\Models\Teacher;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +19,8 @@ class TeacherSpecialityFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'teacher_id' => Teacher::factory(),
+            'speciality_id' => Speciality::factory(),
         ];
     }
 }
