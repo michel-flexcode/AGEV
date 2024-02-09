d992774959692a5a7ba028823fc021d42e4ac7a5
MichelCode <michelseocode.com>
2024-02-09 14:09:44 +0100

    index remanié features-mich-18

--- resources/js/Pages/Questions/Index.vue
+++ resources/js/Pages/Questions/Index.vue
@@ -1,10 +1,47 @@
 <script setup>
 import AppLayout from "@/Layouts/AppLayout.vue";
-import { Link } from "@inertiajs/vue3";
+import { Link, router, useForm } from "@inertiajs/vue3";
+import ActionMessage from "@/Components/ActionMessage.vue";
+import FormSection from "@/Components/FormSection.vue";
+import InputError from "@/Components/InputError.vue";
+import InputLabel from "@/Components/InputLabel.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import SecondaryButton from "@/Components/SecondaryButton.vue";
+import DangerButton from "@/Components/DangerButton.vue";
+import TextInput from "@/Components/TextInput.vue";
+import DialogModal from "@/Components/DialogModal.vue";
+import { ref } from "vue";
 
 const props = defineProps({
     questions: Array,
+    question: Object,
 });
+
+//ajout bloc delete
+const confirmingQuestionDeletion = ref(false);
+const questionIdToDelete = ref(null);
+const formDeleteQuestion = useForm("delete", {});
+
+const confirmQuestionDeletion = (id) => {
+    questionIdToDelete.value = id;
+    confirmingQuestionDeletion.value = true;
+};
+
+const deleteQuestion = () => {
+    formDeleteQuestion.delete(
+        route("questions.destroy", questionIdToDelete.value),
+        {
+            preserveScroll: true,
+            onSuccess: () => {
+                confirmingQuestionDeletion.value = false;
+            },
+        }
+    );
+};
+
+const closeModal = () => {
+    confirmingQuestionDeletion.value = false;
+};
 </script>
 
 <template>
@@ -26,6 +63,15 @@ const props = defineProps({
                         <Link :href="route('questions.edit', question)">
                             {{ question.label }}
                         </Link>
+
+                        <button
+                            @click.prevent="
+                                confirmQuestionDeletion(question.id)
+                            "
+                            class="btn btn-outline-danger"
+                        >
+                            Supprimer
+                        </button>
                     </li>
                 </ul>
                 <div class="mt-4">
@@ -36,4 +82,26 @@ const props = defineProps({
             </div>
         </div>
     </AppLayout>
+
+    <DialogModal :show="confirmingQuestionDeletion" @close="closeModal">
+        <template #title> Supprimer la question </template>
+
+        <template #content>
+            Êtes-vous sûr de vouloir supprimer cette question ? Cette action est
+            irréversible.
+        </template>
+
+        <template #footer>
+            <SecondaryButton @click="closeModal"> Annuler </SecondaryButton>
+
+            <DangerButton
+                class="ms-3"
+                :class="{ 'opacity-25': confirmingQuestionDeletion.processing }"
+                :disabled="confirmingQuestionDeletion.processing"
+                @click="deleteQuestion"
+            >
+                Supprimer
+            </DangerButton>
+        </template>
+    </DialogModal>
 </template>
